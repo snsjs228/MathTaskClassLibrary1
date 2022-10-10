73b52cbef84b9d64b36f3fbc537f8a6d79856186
Krusa <zvekliza2003@gmail.com>
2022-10-10 15:31:42 +0300

    Добавьте файлы проекта.

---
+++ MathTaskClassLibraryTests/GeometryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathTaskClassLibrary1;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTest
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            Geometry g = new Geometry();
+            int actual = g.RectangleArea(a, b);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
